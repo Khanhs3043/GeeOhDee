4b510a40488f32571460c460dbd822b7be69b07a
Khanhs3043 <xuankhanh3043@gmail.com>
2024-03-28 13:32:29 +0700

    fix fav

--- chos/lib/screens/favoriteScreen.dart
+++ chos/lib/screens/favoriteScreen.dart
@@ -1,9 +1,12 @@
+
 import 'package:chos/services/dbHelper.dart';
-import 'package:chos/widgets/breedItem.dart';
+import 'package:chos/widgets/favoriteItem.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../models/dog.dart';
+import '../providers/userProvider.dart';
 class FavoriteScreen extends StatefulWidget {
   const FavoriteScreen({super.key});
 
@@ -19,20 +22,43 @@ class _FavoriteScreenState extends State<FavoriteScreen> {
   }
   loadData()async{
     dogs = await DbHelper.getListFavoriteDog(FirebaseAuth.instance.currentUser!.uid);
-    for(var i in dogs!)
-    print(i.id);
     setState(() {
     });
   }
-  
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.transparent,
-      body: SingleChildScrollView(
-        child: dogs==null?Center(child: CircularProgressIndicator()):Column(
-          children: dogs!.map((e) => BreedItem(dog: e)).toList(),
+
+    return Consumer<UserProvider>(
+      builder: (context,provider,child){
+
+      return Scaffold(
+        body: Container(
+          width: MediaQuery.sizeOf(context).width,
+          height: MediaQuery.sizeOf(context).height,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [Colors.brown,Colors.blueGrey,Colors.white]
+            )
+          ),
+
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                provider.state?SizedBox():SizedBox(),
+                const SizedBox(height: 50,),
+                Text('Your favorite',style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 20),),
+                dogs==null?Center(child: CircularProgressIndicator()):Center(
+                  child: Wrap(
+                    spacing: 20,
+                    children: dogs!.map((e) => FavoriteItem(dog: e)).toList(),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
-      ),
+      );}
     );
   }
 }
---
+++ chos/lib/widgets/favoriteItem.dart
@@ -0,0 +1,71 @@
+import 'package:chos/providers/userProvider.dart';
+import 'package:chos/screens/detailsScreen.dart';
+import 'package:chos/services/dbHelper.dart';
+import 'package:chos/services/dogService.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:provider/provider.dart';
+
+import '../models/dog.dart';
+class FavoriteItem extends StatelessWidget {
+   FavoriteItem({super.key, required this.dog});
+   Dog dog;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top:20,),
+      height: 200,
+      width: MediaQuery.sizeOf(context).width/2-30,
+      decoration: BoxDecoration(
+        color: Colors.blueGrey,
+        image: DecorationImage(
+          image: NetworkImage(DogService.getImageStringByReferenceId(dog.referenceImageId!)),
+          fit: BoxFit.cover,
+        ),
+        borderRadius: BorderRadius.circular(20)
+      ),
+      child: Center(
+        child: GestureDetector(
+          onTap: (){
+            Navigator.push(context, MaterialPageRoute(builder: (context)=>DetailsScreen(dog: dog)));
+          },
+          onLongPress: (){
+            showDialog(context: context, builder: (context)=>AlertDialog(
+              title: null,
+              actions: [TextButton(onPressed: ()async{
+                await DbHelper.removeFavorite(dog.id!, FirebaseAuth.instance.currentUser!.uid);
+                Provider.of<UserProvider>(context,listen: false).state = true;
+                Provider.of<UserProvider>(context,listen: false).change();
+                Navigator.pop(context);
+              }, child: Text('Delete',style: TextStyle(color: Colors.red),)),
+                TextButton(onPressed: (){Navigator.pop(context);}, child: Text('Cancel',style: TextStyle(color: Colors.grey),))],
+            ));
+          },
+          child: Container(
+            padding: EdgeInsets.all(10),
+            width: MediaQuery.sizeOf(context).width,
+            height: MediaQuery.sizeOf(context).height,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              color: Colors.black.withOpacity(0.3),
+
+            ),
+            child: Stack(
+              alignment: Alignment.bottomLeft,
+              children: [
+                Positioned(
+                  top:0,
+                    right: 0,
+                    child: Icon(Icons.favorite,color: Colors.white,)),
+                Expanded(
+                    child: Text('${dog.name}',style: TextStyle(fontSize: 23,fontWeight: FontWeight.bold,color: Colors.white,),)),
+              ],
+            ),
+          ),
+        ),
+      )
+    );
+  }
+}
